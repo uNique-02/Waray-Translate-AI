bb78f094991408b20e31990e85ced00cd309352a
kimz <92148017+uNique-02@users.noreply.github.com>
2025-10-20 19:32:26 +0800

    Fix bugs in rendering of images and duplicate chats

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,9 +10,9 @@ const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 console.log("VITE GOOGLE_CLIENT_ID:", import.meta.env.VITE_GOOGLE_CLIENT_ID);
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
-  <GoogleOAuthProvider clientId={clientId}>
-    <App />
-  </GoogleOAuthProvider>
-  // </StrictMode>
+  <StrictMode>
+    <GoogleOAuthProvider clientId={clientId}>
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>
 );
